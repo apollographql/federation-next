8f02d9bf69894d0c7b89ac7d0cfd442cdca456b1
Renée Kooi <renee.kooi@apollographql.com>
2024-01-04 17:21:37 +0100

    Validate `@inaccessible` on type definitions

--- src/link/inaccessible_spec_definition.rs
+++ src/link/inaccessible_spec_definition.rs
@@ -1,6 +1,8 @@
-use crate::error::{FederationError, SingleFederationError};
+use crate::error::{FederationError, MultipleFederationErrors, SingleFederationError};
 use crate::link::spec::{Identity, Url, Version};
 use crate::link::spec_definition::{SpecDefinition, SpecDefinitions};
+use crate::schema::position::TypeDefinitionPosition;
+use crate::schema::referencer::TypeDefinitionReferencer;
 use crate::schema::FederationSchema;
 use apollo_compiler::name;
 use apollo_compiler::schema::{Directive, Name};
@@ -81,4 +83,104 @@ pub(crate) fn get_inaccessible_spec_definition_from_subgraph(
             message: "Subgraph unexpectedly does not use a supported inaccessible spec version"
                 .to_owned(),
         })?)
+}
+
+pub fn validate_inaccessible(schema: &FederationSchema) -> Result<(), FederationError> {
+    let inaccessible_spec = get_inaccessible_spec_definition_from_subgraph(schema)?;
+    let directive_name = inaccessible_spec
+        .directive_name_in_schema(schema, &INACCESSIBLE_DIRECTIVE_NAME_IN_SPEC)?
+        .ok_or_else(|| SingleFederationError::Internal {
+            message: "Unexpectedly could not find inaccessible spec in schema".to_owned(),
+        })?;
+
+    let mut errors = MultipleFederationErrors { errors: vec![] };
+
+    let referencers = schema.referencers();
+    let type_definitions = referencers.to_type_definition_referencers();
+    for position in schema.get_types() {
+        let Ok(ty) = position.get(schema.schema()) else {
+            continue;
+        };
+        let is_inaccessible = ty.directives().has(&directive_name);
+
+        if !is_inaccessible {
+            // A union that is accessible must have at least 1 accessible member
+            if let TypeDefinitionPosition::Union(union_position) = &position {
+                let union_ = union_position.get(schema.schema())?;
+                let any_accessible_member = union_.members.iter().any(|member| {
+                    !schema
+                        .schema()
+                        .types
+                        .get(&member.name)
+                        .is_some_and(|ty| ty.directives().has("inaccessible"))
+                });
+
+                if !any_accessible_member {
+                    errors.push(SingleFederationError::OnlyInaccessibleChildren {
+                        message: format!("Type `{position}` is in the API schema but all of its members are @inaccessible."),
+                    }.into());
+                }
+            }
+
+            continue;
+        }
+
+        let Some(references) = type_definitions.get(&position) else {
+            continue; // no references, OK to remove
+        };
+
+        for ref_position in references.iter() {
+            let ref_inaccessible = match ref_position {
+                TypeDefinitionReferencer::SchemaRoot(_) => {
+                    errors.push(SingleFederationError::QueryRootTypeInaccessible {
+                        message: format!("Type `{position}` is @inaccessible but is the query root type, which must be in the API schema."),
+                    }.into());
+                    continue;
+                }
+                TypeDefinitionReferencer::Union(_) => {
+                    // This type will be removed from the union
+                    continue;
+                }
+                // General types
+                TypeDefinitionReferencer::Object(ref_position) => ref_position
+                    .get(schema.schema())?
+                    .directives
+                    .has(&directive_name),
+                TypeDefinitionReferencer::ObjectField(ref_position) => ref_position
+                    .get(schema.schema())?
+                    .directives
+                    .has(&directive_name),
+                TypeDefinitionReferencer::ObjectFieldArgument(_) => false,
+                TypeDefinitionReferencer::Interface(ref_position) => ref_position
+                    .get(schema.schema())?
+                    .directives
+                    .has(&directive_name),
+                TypeDefinitionReferencer::InterfaceField(ref_position) => ref_position
+                    .get(schema.schema())?
+                    .directives
+                    .has(&directive_name),
+                TypeDefinitionReferencer::InterfaceFieldArgument(_) => false,
+                TypeDefinitionReferencer::UnionField(ref_position) => ref_position
+                    .get(schema.schema())?
+                    .directives
+                    .has(&directive_name),
+                TypeDefinitionReferencer::InputObjectField(ref_position) => ref_position
+                    .get(schema.schema())?
+                    .directives
+                    .has(&directive_name),
+                TypeDefinitionReferencer::DirectiveArgument(_) => false,
+            };
+            if !ref_inaccessible {
+                errors.push(SingleFederationError::ReferencedInaccessible {
+                    message: format!("Type `{position}` is @inaccessible but is referenced by `{ref_position}`, which is in the API schema."),
+                }.into());
+            }
+        }
+    }
+
+    if !errors.errors.is_empty() {
+        return Err(errors.into());
+    }
+
+    todo!()
 }
