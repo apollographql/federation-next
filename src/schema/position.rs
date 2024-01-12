98c63d0532c45f44094ccef9164e0d82674d0319
Renée Kooi <renee.kooi@apollographql.com>
2024-01-12 13:46:57 +0100

    Revert "Fix error while removing directive argument that refers to a type that no longer exists"
    This reverts commit 082e00e38b060b79c094e0b35c2bfc1fc5988ddc.

--- src/schema/position.rs
+++ src/schema/position.rs
@@ -6058,7 +6058,15 @@ impl DirectiveArgumentDefinitionPosition {
                     .remove(self);
             }
             _ => {
-                // Already gone
+                return Err(
+                    SingleFederationError::Internal {
+                        message: format!(
+                            "Directive argument \"{}\"'s inner type \"{}\" does not refer to an existing input type.",
+                            self,
+                            input_type_reference.deref(),
+                        )
+                    }.into()
+                );
             }
         }
         Ok(())
