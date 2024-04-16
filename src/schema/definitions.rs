cd259d32315a9f473430369af341639f3b0e3c03
Simon Sapin <simon@apollographql.com>
2024-04-16 21:37:45 +0200

    Fix types_can_be_merged logic

--- src/schema/definitions.rs
+++ src/schema/definitions.rs
@@ -14,11 +14,6 @@ pub(crate) enum AbstractType {
     Union(UnionTypeDefinitionPosition),
 }
 
-pub(crate) enum WrapperType {
-    List(Type),
-    NonNull(Type),
-}
-
 pub(crate) fn is_abstract_type(ty: TypeDefinitionPosition) -> bool {
     matches!(
         ty,
@@ -52,56 +47,19 @@ pub(crate) fn types_can_be_merged(
     t2: &Type,
     schema: &Schema,
 ) -> Result<bool, FederationError> {
-    if t1.is_non_null() {
-        return Ok(if t2.is_non_null() {
-            types_can_be_merged(&(t1.clone().nullable()), &(t2.clone().nullable()), schema)?
-        } else {
-            false
-        });
-    }
-    if t1.is_list() {
-        return Ok(if t2.is_list() {
-            types_can_be_merged(t1.item_type(), t2.item_type(), schema)?
-        } else {
-            false
-        });
-    }
-
-    if is_composite_type(t1.inner_named_type(), schema)? {
-        return is_composite_type(t2.inner_named_type(), schema);
+    let (n1, n2) = match (t1, t2) {
+        (Type::Named(n1), Type::Named(n2)) | (Type::NonNullNamed(n1), Type::NonNullNamed(n2)) => {
+            (n1, n2)
+        }
+        (Type::List(inner1), Type::List(inner2))
+        | (Type::NonNullList(inner1), Type::NonNullList(inner2)) => {
+            return types_can_be_merged(inner1, inner2, schema)
+        }
+        _ => return Ok(false),
+    };
+    if is_composite_type(n1, schema)? {
+        return is_composite_type(n2, schema);
     }
 
-    Ok(same_type(t1, t2))
-}
-
-/**
- * Tests whether 2 types are the "same" type.
- *
- * To be the same type, for this method, is defined as having the samee name for named types
- * or, for wrapper types, the same wrapper type and recursively same wrapped one.
- *
- * This method does not check that both types are from the same schema and does not validate
- * that the structure of named types is the same. Also note that it does not check the "kind"
- * of the type, which is actually relied on due to @interfaceObject (where the "same" type
- * can be an interface in one subgraph but an object type in another, while fundamentally being
- * the same type).
- */
-pub(crate) fn same_type(t1: &Type, t2: &Type) -> bool {
-    if t1.is_list() {
-        return if t2.is_list() {
-            same_type(t1.item_type(), t2.item_type())
-        } else {
-            false
-        };
-    }
-
-    if t1.is_non_null() {
-        return if t2.is_non_null() {
-            same_type(&(t1.clone().nullable()), &(t2.clone().nullable()))
-        } else {
-            false
-        };
-    }
-
-    t1.inner_named_type() == t2.inner_named_type()
+    Ok(n1 == n2)
 }
