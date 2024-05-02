f0158ef0067d2dfebce85caa59b4a4a99fc853da
Nicholas Cioli <nicholas.cioli@apollographql.com>
2024-05-02 18:12:26 -0400

    add source ID support to mock

--- src/sources/connect/query_graph.rs
+++ src/sources/connect/query_graph.rs
@@ -12,7 +12,7 @@ use crate::{
     sources::{
         connect::ConnectFederatedScalarQueryGraphNode, SourceFederatedConcreteFieldQueryGraphEdge,
         SourceFederatedConcreteQueryGraphNode, SourceFederatedEnumQueryGraphNode,
-        SourceFederatedQueryGraphBuilderApi, SourceFederatedScalarQueryGraphNode,
+        SourceFederatedQueryGraphBuilderApi, SourceFederatedScalarQueryGraphNode, SourceId,
     },
     ValidFederationSubgraph,
 };
@@ -31,12 +31,12 @@ impl SourceFederatedQueryGraphBuilderApi for ConnectFederatedQueryGraphBuilder {
         builder: &mut impl IntraSourceQueryGraphBuilderApi,
     ) -> Result<(), FederationError> {
         // Extract the connects from the schema definition and map them to their `Connect` equivalent
-        // Note: We need to keep the object_field as well as the arguments since it contains important
-        // type information of the original schema.
-        // TODO: Validate that we don't have connect directives on anything but object fields.
         let connectors = Connector::from_valid_schema(&subgraph.schema, subgraph.name.into())?;
 
         for (id, connect) in connectors {
+            // Inform the builder that every node / edge from here out are part of the current connect directive
+            builder.add_and_set_current_source(SourceId::Connect(id.clone()))?;
+
             let ObjectOrInterfaceFieldDefinitionPosition::Object(field_def_pos) =
                 id.directive.field
             else {
@@ -413,21 +413,37 @@ mod tests {
         let as_dot = mock_builder.into_dot();
         assert_snapshot!(as_dot, @r###"
         digraph {
-            0 [ label = "Node: Query" ]
-            1 [ label = "Node: User" ]
-            2 [ label = "Scalar: ID" ]
-            3 [ label = "Scalar: String" ]
-            4 [ label = "Node: Query" ]
-            5 [ label = "Node: Post" ]
-            6 [ label = "Scalar: ID" ]
-            7 [ label = "Scalar: String" ]
-            1 -> 2 [ label = "id" ]
-            1 -> 3 [ label = "name" ]
-            0 -> 1 [ label = "users" ]
-            5 -> 6 [ label = "id" ]
-            5 -> 7 [ label = "title" ]
-            5 -> 7 [ label = "body" ]
-            4 -> 5 [ label = "posts" ]
+          subgraph cluster_0 {
+            node [style = filled,color = white]
+
+            0.0 [ label = "Node: Query" ]
+            0.1 [ label = "Node: User" ]
+            0.2 [ label = "Scalar: ID" ]
+            0.3 [ label = "Scalar: String" ]
+
+            style = filled
+            color = lightgrey
+            label = "Query.users"
+          }
+          subgraph cluster_1 {
+            node [style = filled,color = white]
+
+            1.0 [ label = "Node: Query" ]
+            1.1 [ label = "Node: Post" ]
+            1.2 [ label = "Scalar: ID" ]
+            1.3 [ label = "Scalar: String" ]
+
+            style = filled
+            color = lightgrey
+            label = "Query.posts"
+          }
+          0.1 -> 0.2 [ label = "id" ]
+          0.1 -> 0.3 [ label = "name" ]
+          0.0 -> 0.1 [ label = "users" ]
+          1.1 -> 1.2 [ label = "id" ]
+          1.1 -> 1.3 [ label = "title" ]
+          1.1 -> 1.3 [ label = "body" ]
+          1.0 -> 1.1 [ label = "posts" ]
         }
         "###);
     }
@@ -448,21 +464,37 @@ mod tests {
         let as_dot = mock_builder.into_dot();
         assert_snapshot!(as_dot, @r###"
         digraph {
-            0 [ label = "Node: Query" ]
-            1 [ label = "Node: User" ]
-            2 [ label = "Scalar: ID" ]
-            3 [ label = "Scalar: String" ]
-            4 [ label = "Node: Query" ]
-            5 [ label = "Node: Post" ]
-            6 [ label = "Scalar: ID" ]
-            7 [ label = "Scalar: String" ]
-            1 -> 2 [ label = "id" ]
-            1 -> 3 [ label = "name: .username" ]
-            0 -> 1 [ label = "users" ]
-            5 -> 6 [ label = "id" ]
-            5 -> 7 [ label = "title: .\"body title\"" ]
-            5 -> 7 [ label = "body: .summary" ]
-            4 -> 5 [ label = "posts" ]
+          subgraph cluster_0 {
+            node [style = filled,color = white]
+
+            0.0 [ label = "Node: Query" ]
+            0.1 [ label = "Node: User" ]
+            0.2 [ label = "Scalar: ID" ]
+            0.3 [ label = "Scalar: String" ]
+
+            style = filled
+            color = lightgrey
+            label = "Query.users"
+          }
+          subgraph cluster_1 {
+            node [style = filled,color = white]
+
+            1.0 [ label = "Node: Query" ]
+            1.1 [ label = "Node: Post" ]
+            1.2 [ label = "Scalar: ID" ]
+            1.3 [ label = "Scalar: String" ]
+
+            style = filled
+            color = lightgrey
+            label = "Query.posts"
+          }
+          0.1 -> 0.2 [ label = "id" ]
+          0.1 -> 0.3 [ label = "name: .username" ]
+          0.0 -> 0.1 [ label = "users" ]
+          1.1 -> 1.2 [ label = "id" ]
+          1.1 -> 1.3 [ label = "title: .\"body title\"" ]
+          1.1 -> 1.3 [ label = "body: .summary" ]
+          1.0 -> 1.1 [ label = "posts" ]
         }
         "###
         );
@@ -484,26 +516,42 @@ mod tests {
         let as_dot = mock_builder.into_dot();
         assert_snapshot!(as_dot, @r###"
         digraph {
-            0 [ label = "Node: Query" ]
-            1 [ label = "Node: User" ]
-            2 [ label = "Scalar: ID" ]
-            3 [ label = "Scalar: String" ]
-            4 [ label = "Node: User" ]
-            5 [ label = "Node: Query" ]
-            6 [ label = "Node: User" ]
-            7 [ label = "Scalar: ID" ]
-            8 [ label = "Scalar: String" ]
-            9 [ label = "Node: User" ]
-            1 -> 2 [ label = "id" ]
-            1 -> 3 [ label = "name" ]
-            4 -> 2 [ label = "id" ]
-            1 -> 4 [ label = "friends" ]
-            0 -> 1 [ label = "me" ]
-            6 -> 7 [ label = "id" ]
-            6 -> 8 [ label = "name" ]
-            9 -> 7 [ label = "id" ]
-            6 -> 9 [ label = "friends" ]
-            5 -> 6 [ label = "user" ]
+          subgraph cluster_0 {
+            node [style = filled,color = white]
+
+            0.0 [ label = "Node: Query" ]
+            0.1 [ label = "Node: User" ]
+            0.2 [ label = "Scalar: ID" ]
+            0.3 [ label = "Scalar: String" ]
+            0.4 [ label = "Node: User" ]
+
+            style = filled
+            color = lightgrey
+            label = "Query.me"
+          }
+          subgraph cluster_1 {
+            node [style = filled,color = white]
+
+            1.0 [ label = "Node: Query" ]
+            1.1 [ label = "Node: User" ]
+            1.2 [ label = "Scalar: ID" ]
+            1.3 [ label = "Scalar: String" ]
+            1.4 [ label = "Node: User" ]
+
+            style = filled
+            color = lightgrey
+            label = "Query.user"
+          }
+          0.1 -> 0.2 [ label = "id" ]
+          0.1 -> 0.3 [ label = "name" ]
+          0.4 -> 0.2 [ label = "id" ]
+          0.1 -> 0.4 [ label = "friends" ]
+          0.0 -> 0.1 [ label = "me" ]
+          1.1 -> 1.2 [ label = "id" ]
+          1.1 -> 1.3 [ label = "name" ]
+          1.4 -> 1.2 [ label = "id" ]
+          1.1 -> 1.4 [ label = "friends" ]
+          1.0 -> 1.1 [ label = "user" ]
         }
         "###
         );
@@ -525,25 +573,33 @@ mod tests {
         let as_dot = mock_builder.into_dot();
         assert_snapshot!(as_dot, @r###"
         digraph {
-            0 [ label = "Node: Query" ]
-            1 [ label = "Node: User" ]
-            2 [ label = "Scalar: ID" ]
-            3 [ label = "Node: UserInfo" ]
-            4 [ label = "Scalar: String" ]
-            5 [ label = "Node: UserAddress" ]
-            6 [ label = "Scalar: Int" ]
-            7 [ label = "Node: UserAvatar" ]
-            1 -> 2 [ label = "id" ]
-            3 -> 4 [ label = "name: .\"user full name\"" ]
-            5 -> 4 [ label = "street: .street_line" ]
-            5 -> 4 [ label = "state" ]
-            5 -> 6 [ label = "zip" ]
-            3 -> 5 [ label = "address: .addresses.main.address" ]
-            7 -> 4 [ label = "large" ]
-            7 -> 4 [ label = "thumbnail" ]
-            3 -> 7 [ label = "avatar" ]
-            1 -> 3 [ label = "info: .user_info" ]
-            0 -> 1 [ label = "user" ]
+          subgraph cluster_0 {
+            node [style = filled,color = white]
+
+            0.0 [ label = "Node: Query" ]
+            0.1 [ label = "Node: User" ]
+            0.2 [ label = "Scalar: ID" ]
+            0.3 [ label = "Node: UserInfo" ]
+            0.4 [ label = "Scalar: String" ]
+            0.5 [ label = "Node: UserAddress" ]
+            0.6 [ label = "Scalar: Int" ]
+            0.7 [ label = "Node: UserAvatar" ]
+
+            style = filled
+            color = lightgrey
+            label = "Query.user"
+          }
+          0.1 -> 0.2 [ label = "id" ]
+          0.3 -> 0.4 [ label = "name: .\"user full name\"" ]
+          0.5 -> 0.4 [ label = "street: .street_line" ]
+          0.5 -> 0.4 [ label = "state" ]
+          0.5 -> 0.6 [ label = "zip" ]
+          0.3 -> 0.5 [ label = "address: .addresses.main.address" ]
+          0.7 -> 0.4 [ label = "large" ]
+          0.7 -> 0.4 [ label = "thumbnail" ]
+          0.3 -> 0.7 [ label = "avatar" ]
+          0.1 -> 0.3 [ label = "info: .user_info" ]
+          0.0 -> 0.1 [ label = "user" ]
         }
         "###
         );
@@ -554,8 +610,9 @@ mod tests {
 
         use apollo_compiler::ast::{Name, NamedType};
         use indexmap::IndexSet;
+        use itertools::Itertools;
         use petgraph::{
-            dot::Dot,
+            dot::{Config, Dot},
             prelude::{EdgeIndex, NodeIndex},
             Graph,
         };
@@ -578,9 +635,11 @@ mod tests {
         };
 
         /// A mock query Node
+        #[derive(Clone)]
         struct MockNode {
             prefix: String,
             type_name: NamedType,
+            source_id: SourceId,
         }
         impl Display for MockNode {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -631,20 +690,146 @@ mod tests {
         /// Mock implementation of [IntraSourceQueryGraphBuilder]
         pub struct MockSourceQueryGraphBuilder {
             graph: Graph<MockNode, MockEdge>,
+            current_source: Option<SourceId>,
         }
         impl MockSourceQueryGraphBuilder {
             pub fn new() -> Self {
                 Self {
                     graph: Graph::new(),
+                    current_source: None,
                 }
             }
 
+            /// Export the graph as a [dot]() file.
+            ///
+            /// Note: PetGraph does not support subgraphs, so this method does a few
+            /// hacks to manually join the generated dots into one combined file.
             pub fn into_dot(self) -> String {
-                Dot::with_config(&self.graph, &[]).to_string()
+                // Collect all nodes, grouped by source ID
+                let nodes_by_source_id = self
+                    .graph
+                    .raw_nodes()
+                    .iter()
+                    .group_by(|node| node.weight.source_id.clone());
+                let edges = self.graph.raw_edges();
+
+                // Create graphs for each set of nodes, and immediately export it as a dot file
+                let subgraphs =
+                    nodes_by_source_id
+                        .into_iter()
+                        .enumerate()
+                        .map(|(index, (id, nodes))| {
+                            let subgraph = nodes.fold(
+                                Graph::new(),
+                                |mut acc: Graph<MockNode, MockEdge>, node| {
+                                    acc.add_node(node.weight.clone());
+                                    acc
+                                },
+                            );
+
+                            // Grab the contents of the dot file
+                            let dot = Dot::with_config(&subgraph, &[Config::GraphContentOnly])
+                                .to_string();
+
+                            // Pair the unique ID of the connect directive with the rendered subgraph, stripping any extra whitespace
+                            // for later formatting.
+                            //
+                            // Note: We also prefix the nodes with their subgraph index so that they are unique within the context
+                            // of the entire graph.
+                            let SourceId::Connect(connect_id) = id else {
+                                unreachable!()
+                            };
+
+                            (
+                                connect_id.directive.field.to_string(),
+                                dot.lines()
+                                    .map(|line| format!("{index}.{}", line.trim()))
+                                    .join("\n"),
+                            )
+                        });
+
+                // We'll need to keep track of how many nodes are in each subgraph so that we can manually construct the
+                // edges after using these counts as a way to caluclate the offset.
+                let mut counts = Vec::new();
+
+                // Render the final graph
+                // Something like `indoc!` would be super useful here...
+                let mut combined_dot = String::new();
+                combined_dot.push_str("digraph {\n");
+
+                for (index, (id, dot)) in subgraphs.enumerate() {
+                    counts.push(dot.lines().count());
+
+                    combined_dot.extend([
+                        &format!("  subgraph cluster_{index} {{\n"),
+                        "    node [style = filled,color = white]\n",
+                        "\n",
+                    ]);
+                    combined_dot.extend(dot.lines().map(|line| format!("    {line}\n")));
+                    combined_dot.extend([
+                        "\n",
+                        "    style = filled\n",
+                        "    color = lightgrey\n",
+                        &format!("    label = \"{id}\"\n"),
+                    ]);
+                    combined_dot.push_str("  }\n");
+                }
+
+                // Map the counts now to be the total running count from the index
+                let len = counts.len();
+                counts = counts
+                    .into_iter()
+                    .fold(Vec::with_capacity(len), |mut acc, next| {
+                        let last = acc.last().unwrap_or(&0);
+                        acc.push(last + next);
+
+                        acc
+                    });
+
+                // Helper to get the offset and index from a node ID's index
+                // Note: Reversing the counts here makes it easier to search
+                counts.reverse();
+                let offset_and_index = move |index: usize| -> (usize, usize) {
+                    let last_max_pos = counts.iter().position(|&count| count <= index);
+
+                    if let Some(last_max) = last_max_pos {
+                        (last_max, index - counts[last_max])
+                    } else {
+                        (0, index)
+                    }
+                };
+
+                // Add in the edges
+                for edge in edges {
+                    let (from_offset, from) = offset_and_index(edge.source().index());
+                    let (to_offset, to) = offset_and_index(edge.target().index());
+
+                    combined_dot.push_str(&format!(
+                        "  {}.{} -> {}.{} [ label = \"{}\" ]\n",
+                        from_offset,
+                        from,
+                        to_offset,
+                        to,
+                        edge.weight.to_string().replace('"', "\\\"")
+                    ));
+                }
+
+                // Finish off the graph
+                combined_dot.push('}');
+                combined_dot
             }
         }
 
         impl IntraSourceQueryGraphBuilderApi for MockSourceQueryGraphBuilder {
+            fn add_and_set_current_source(
+                &mut self,
+                source: SourceId,
+            ) -> Result<(), FederationError> {
+                self.current_source = Some(source);
+
+                Ok(())
+            }
+
             // We only support concrete types for now
             fn add_concrete_node(
                 &mut self,
@@ -658,6 +843,7 @@ mod tests {
                 Ok(self.graph.add_node(MockNode {
                     prefix: "Node".to_string(),
                     type_name: supergraph_type_name,
+                    source_id: self.current_source.as_ref().unwrap().clone(),
                 }))
             }
 
@@ -698,6 +884,7 @@ mod tests {
                 Ok(self.graph.add_node(MockNode {
                     prefix: "Scalar".to_string(),
                     type_name: supergraph_type_name,
+                    source_id: self.current_source.as_ref().unwrap().clone(),
                 }))
             }
 
@@ -708,13 +895,6 @@ mod tests {
             fn source_query_graph(
                 &mut self,
             ) -> Result<&mut SourceFederatedQueryGraph, FederationError> {
-                todo!()
-            }
-
-            fn add_and_set_current_source(
-                &mut self,
-                _source: SourceId,
-            ) -> Result<(), FederationError> {
                 todo!()
             }
 
